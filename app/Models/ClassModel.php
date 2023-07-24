c5b77d56ec481a900f535d848a0148782af988a4
JavohirMamaniyazov <javohirmamaniyazov@gmail.com>
2023-07-24 07:10:28 +0500

    have some works in ClassModel

--- app/Models/ClassModel.php
+++ app/Models/ClassModel.php
@@ -13,15 +13,8 @@ class ClassModel extends Model
 
     static public function getrecord()
     {
-        $query = ClassModel::select("users.*", "users.name as created_by_name")
-            ->join("users", "users.id", "=", "class.created_by")
-            ->orderBy("class.id", "desc");
+        $data = ClassModel::select("users.*", "users.name as created_by_name")->join("users", "users.id", "class.created_by")->orderBy("class.id", "desc");
 
-        // Paginate the results with a specified number of records per page.
-        $perPage = 10; // Change this number according to your requirements.
-        $paginatedData = $query->paginate($perPage);
-
-        return $paginatedData;
+        return $data;
     }
-
 }
